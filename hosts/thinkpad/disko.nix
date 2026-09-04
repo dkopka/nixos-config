54b5f09c25b0cc1ddbc7367371ab94eec5711998
Dariusz Kopka <dkopka@plume.com>
2026-09-04 23:09:15 +0200

    Fixes for disko (disable) and hardware.nix
    Signed-off-by: Dariusz Kopka <dkopka@plume.com>

--- hosts/thinkpad/disko.nix
+++ hosts/thinkpad/disko.nix
@@ -1,7 +1,13 @@
+{ inputs, ... }:
 let
   ext4 = mountpoint: { type = "filesystem"; format = "ext4"; inherit mountpoint; };
 in
 {
+  imports = [inputs.disko.nixosModules.disko ];
+  # FIXME: We run this on existing system that doesn't
+  # require disko configuration. Change to 'true' for
+  # fresh installs.
+  disko.enableConfig = false;
   disko.devices = {
     disk.main = {
       type = "disk";
