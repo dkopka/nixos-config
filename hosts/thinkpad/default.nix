0e323c0e62a8535d3ff03f4d18bfea0add59673f
Dariusz Kopka <dkopka@plume.com>
2026-09-04 21:58:37 +0200

    TEST: Added hardware configuration from nixos-hardware
    Signed-off-by: Dariusz Kopka <dkopka@plume.com>

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -4,5 +4,9 @@
 
     ../../private/hardware-configuration.nix
     ../../private/luks.nix
+
+    modules = [
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p14s-intel-gen2
+    ];
   ];
 }
